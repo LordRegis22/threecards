31412c2e03076335de92e88b0c2f238ee3788abf
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-06 11:35:54 -0700

    Add global error handler for middleware errors

--- server/server.js
+++ server/server.js
@@ -11,4 +11,17 @@ app.use('/', express.static(path.resolve(__dirname, '../client')));
 app.use((req, res) => {
   res.status(404).send("Can't find anything here!");
 });
+
+app.use((err, req, res, next) => {
+  //global error handling here
+  const defaultErr = {
+    log: 'Global error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred in middleware' },
+  };
+  let errorObj = Object.assign({}, defaultErr);
+  errorObj.message.err = err;
+  console.log(errorObj.log, errorObj.message);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 app.listen(PORT, console.log(`listening on ${PORT}`));
