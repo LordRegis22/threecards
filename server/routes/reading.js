0d15016a83064acaa64f7da4f5ef55ee0a0e99cb
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-07 18:37:15 -0700

    Send image of card

--- server/routes/reading.js
+++ server/routes/reading.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const readingController = require('../controllers/readingController');
 
 router.use('/', readingController.newCard, (req, res) =>
-  res.status(200).json('new Card created ya fool!')
+  res.status(200).json(res.locals.image)
 );
 
 module.exports = router;
