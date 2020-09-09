d9ec41fb7f24cbecd4d5378a71a20318c7fe047d
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-09 00:25:56 -0700

    Create state map and dispatch map

--- client/containers/MainContainer.js
+++ client/containers/MainContainer.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import * as userActions from '../../actions/userActions';
+import * as readingActions from '../../actions/readingActions';
 import ReadingPanel from '../components/ReadingPanel.jsx';
 import LoginPanel from '../components/LoginPanel.jsx';
+
+const mapStateToProps = (state) => ({ loggedinUser: state.user.loggedinUser });
+
+const mapDispatchToProps = (dispatch) => ({
+  setNewUsername: (input) => dispatch(userActions.setNewUsername(input)),
+});
 
 export class MainContainer extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    const { loggedinUser } = this.props;
-    return <>{loggedinUser ? <ReadingPanel /> : <LoginPanel />}</>;
+    const { loggedinUser, setNewUsername } = this.props;
+    return (
+      <>
+        <LoginPanel setNewUsername={setNewUsername} />
+      </>
+    );
   }
 }
-
-const mapStateToProps = (state) => ({ loggedinUser: state.users.loggedinUser });
-
-const mapDispatchToProps = {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(MainContainer);
