da6bbbb496270885760c749dc95d9b7224d4776d
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-09 14:35:56 -0700

    Remove zombie code

--- client/App.jsx
+++ client/App.jsx
@@ -2,22 +2,6 @@ import React, { useState } from 'react';
 import MainContainer from './containers/MainContainer';
 
 function App() {
-  // const [loggedIn, setLoggedIn] = useState(false);
-  // const [username, setUsername] = useState('');
-  // const [password, setPassword] = useState('');
-
-  // function handleUserChange(e) {
-  //   setUsername(e.target.value);
-  // }
-
-  // function handlePasswordChange(e) {
-  //   setPassword(e.target.value);
-  // }
-
-  // function login() {
-  //   setLoggedIn(true);
-  // }
-
   return <MainContainer />;
 }
 
