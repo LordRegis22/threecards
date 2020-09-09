2227ba1533153958d663defff0133e8c7fb92cf4
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-09 14:35:47 -0700

    Move files into client dir

--- constants/actionTypes.js
+++
@@ -1,8 +0,0 @@
-// user actions
-export const SET_NEW_USERNAME = 'SET_NEW_USERNAME';
-export const SET_NEW_PASSWORD = 'SET_NEW_PASSWORD';
-export const SET_USERNAME = 'SET_USERNAME';
-export const SET_PASSWORD = 'SET_PASSWORD';
-export const SET_LOGGEDIN_USER = 'SET_LOGGEDIN_USER';
-
-//reading actions
