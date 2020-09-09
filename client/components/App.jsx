6002e8e1af1eb0d43af6a8258b7e0ab90aaa543e
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-08 21:39:59 -0700

    Create App component

---
+++ client/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CardTable from './CardTable.jsx';
+
+function App() {
+  return (
+    <>
+      <h1>This is a react Application</h1>
+      <CardTable></CardTable>
+    </>
+  );
+}
+
+export default App;
