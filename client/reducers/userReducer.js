5760b69e1d0ab936f2c7f92cbf48ff55dbb942b1
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-09 10:06:37 -0700

    Create all userReducer methods

--- client/reducers/userReducer.js
+++ client/reducers/userReducer.js
@@ -1,24 +1,41 @@
 import * as types from '../../constants/actionTypes';
 
 const initialState = {
-  newUsername: '',
-  newPassword: '',
   username: '',
   password: '',
+  newUsername: '',
+  newPassword: '',
+  newNickname: '',
   loggedinUser: {},
 };
 
 const userReducer = (state = initialState, action) => {
   let updatedNewUsername;
   let updatedNewPassword;
+  let updatedNewNickname;
   let updatedUsername;
   let updatedPassword;
   let updatedLoggedinUser;
 
   switch (action.type) {
+    case types.SET_USERNAME:
+      updatedUsername = action.payload;
+      return { ...state, username: updatedUsername };
+    case types.SET_PASSWORD:
+      updatedPassword = action.payload;
+      return { ...state, password: updatedPassword };
     case types.SET_NEW_USERNAME:
       updatedNewUsername = action.payload;
       return { ...state, newUsername: updatedNewUsername };
+    case types.SET_NEW_PASSWORD:
+      updatedNewPassword = action.payload;
+      return { ...state, newPassword: updatedNewPassword };
+    case types.SET_NEW_NICKNAME:
+      updatedNewNickname = action.payload;
+      return { ...state, newNickname: updatedNewNickname };
+    case types.SET_LOGGEDIN_USER:
+      updatedLoggedinUser = action.payload;
+      return { ...state, loggedinUser: updatedLoggedinUser };
     default:
       return state;
   }
