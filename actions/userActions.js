e8204cdffb2a2fe2f3cd48e351666ed2e68146de
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-09 00:25:12 -0700

    Create userActions

---
+++ actions/userActions.js
@@ -0,0 +1,26 @@
+import * as types from '../constants/actionTypes';
+
+export const setNewUsername = (newUsername) => ({
+  type: types.SET_NEW_USERNAME,
+  payload: newUsername,
+});
+
+export const setNewPassword = (newPassword) => ({
+  type: types.SET_NEW_PASSWORD,
+  payload: newPassword,
+});
+
+export const setUsername = (username) => ({
+  type: types.SET_USERNAME,
+  payload: username,
+});
+
+export const setPassword = (password) => ({
+  type: types.SET_PASSWORD,
+  payload: password,
+});
+
+export const setLoggedinUser = (user) => ({
+  type: types.SET_LOGGEDIN_USER,
+  payload: user,
+});
