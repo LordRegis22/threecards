5b62355d96046307051d8d166ba8ef97b0d4876d
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-09 00:27:02 -0700

    Change App to wrapper for mainContainer

---
+++ client/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import MainContainer from './containers/MainContainer';
+
+function App() {
+  // const [loggedIn, setLoggedIn] = useState(false);
+  // const [username, setUsername] = useState('');
+  // const [password, setPassword] = useState('');
+
+  // function handleUserChange(e) {
+  //   setUsername(e.target.value);
+  // }
+
+  // function handlePasswordChange(e) {
+  //   setPassword(e.target.value);
+  // }
+
+  // function login() {
+  //   setLoggedIn(true);
+  // }
+
+  return <MainContainer />;
+}
+
+export default App;
