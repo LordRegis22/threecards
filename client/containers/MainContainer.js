169ce5d980c46ea97cfa84918e328044201b33a8
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-09 20:50:13 -0700

    Conditionally render login or reading containers

--- client/containers/MainContainer.js
+++ client/containers/MainContainer.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import * as userActions from '../../actions/userActions';
-import * as readingActions from '../../actions/readingActions';
-import ReadingPanel from '../components/ReadingPanel.jsx';
+import * as userActions from '../actions/userActions';
+import * as readingActions from '../actions/readingActions';
+import ReadingContainer from '../containers/ReadingContainer';
 import LoginContainer from './LoginContainer';
 
 const mapStateToProps = (state) => ({ loggedinUser: state.user.loggedinUser });
 
 const mapDispatchToProps = (dispatch) => ({
-  setNewUsername: (input) => dispatch(userActions.setNewUsername(input)),
+  setLoggedinUser: (userInfo) =>
+    dispatch(userActions.setLoggedinUser(userInfo)),
 });
 
 export class MainContainer extends Component {
@@ -16,10 +17,21 @@ export class MainContainer extends Component {
     super(props);
   }
   render() {
+    const { loggedinUser, setLoggedinUser } = this.props;
     return (
       <>
         <h1>Main Container</h1>
-        <LoginContainer />
+        {/* {loggedinUser.username ? ( */}
+        <ReadingContainer
+          loggedinUser={loggedinUser}
+          setLoggedinUser={setLoggedinUser}
+        />
+        {/* ) : (
+          <LoginContainer
+            setLoggedinUser={setLoggedinUser}
+            loggedinUser={loggedinUser}
+          />
+        )} */}
       </>
     );
   }
