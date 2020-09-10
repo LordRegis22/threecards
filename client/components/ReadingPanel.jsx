ce06014de3bb71e075ba63ead692a45546d4eb37
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-10 09:39:46 -0700

    Add containing elements

--- client/components/ReadingPanel.jsx
+++ client/components/ReadingPanel.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import Card from './Card.jsx';
 
 export default function ReadingPanel(props) {
+  const { showAnswer, displayAnswer } = props;
   return (
-    <div className='reading-panel'>
-      <Card cardNumber={props.keys[0]} />
-      <Card cardNumber={props.keys[1]} />
-      <Card cardNumber={props.keys[2]} />
-    </div>
+    <>
+      <div className='reading-panel'>
+        <div className='card-container'>
+          <Card cardNumber={props.keys[0]} showAnswer={showAnswer} />
+          <div className='keywords'></div>
+        </div>
+        <div className='card-container'>
+          <Card cardNumber={props.keys[1]} showAnswer={showAnswer} />
+          <div className='keywords'></div>
+        </div>
+        <div className='card-container'>
+          <Card cardNumber={props.keys[2]} showAnswer={showAnswer} />
+          <div className='keywords'></div>
+        </div>
+      </div>
+      <div className='divine-answer'>
+        {displayAnswer === 3 && <div className='answer'>ANSWSER!!</div>}
+      </div>
+    </>
   );
 }
