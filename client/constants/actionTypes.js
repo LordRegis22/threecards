4bfe45276e2106ba34a2a0739932ac68f8e8a308
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-09 20:49:41 -0700

    Create all actiontypes

---
+++ client/constants/actionTypes.js
@@ -0,0 +1,15 @@
+// user actions
+export const SET_NEW_USERNAME = 'SET_NEW_USERNAME';
+export const SET_NEW_PASSWORD = 'SET_NEW_PASSWORD';
+export const SET_USERNAME = 'SET_USERNAME';
+export const SET_PASSWORD = 'SET_PASSWORD';
+export const SET_NEW_NICKNAME = 'SET_NEW_NICKNAME';
+export const SET_LOGGEDIN_USER = 'SET_LOGGEDIN_USER';
+
+//reading actions
+export const SHUFFLE = 'SHUFFLE';
+export const DEAL = 'DEAL';
+export const SET_ANSWER = 'SET_ANSWER';
+export const SET_QUESTION = 'SET_QUESTION';
+export const SET_NOTES = 'SET_NOTES';
+export const SAVE_READING = 'SAVE_READING';
