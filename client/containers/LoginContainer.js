6c7b299fd503ba5ca9a81a4fc73d223fd6df5e0d
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-09 20:49:58 -0700

    Map redux store to props

--- client/containers/LoginContainer.js
+++ client/containers/LoginContainer.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import * as userActions from '../../actions/userActions';
+import * as userActions from '../actions/userActions';
 import LoginPanel from '../components/LoginPanel.jsx';
 import RegisterPanel from '../components/RegisterPanel.jsx';
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  username: state.user.username,
+  password: state.user.password,
+  newUsername: state.user.newUsername,
+  newPassword: state.user.newPassword,
+  newNickname: state.user.newNickname,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   setUsername: (input) => dispatch(userActions.setUsername(input)),
@@ -12,6 +18,7 @@ const mapDispatchToProps = (dispatch) => ({
   setNewUsername: (input) => dispatch(userActions.setNewUsername(input)),
   setNewPassword: (input) => dispatch(userActions.setNewPassword(input)),
   setNewNickname: (input) => dispatch(userActions.setNewNickname(input)),
+  setLoggedinUser: (input) => dispatch(userActions.setLoggedinUser(input)),
 });
 
 export class LoginContainer extends Component {
@@ -20,21 +27,37 @@ export class LoginContainer extends Component {
   }
   render() {
     const {
+      username,
+      password,
       setUsername,
       setPassword,
+      newUsername,
+      newPassword,
+      newNickname,
       setNewUsername,
       setNewPassword,
       setNewNickname,
+      setLoggedinUser,
     } = this.props;
 
     return (
       <>
         <h1>Login Container</h1>
-        <LoginPanel setUsername={setUsername} setPassword={setPassword} />
+        <LoginPanel
+          username={username}
+          password={password}
+          setUsername={setUsername}
+          setPassword={setPassword}
+          setLoggedinUser={setLoggedinUser}
+        />
         <RegisterPanel
+          newUsername={newUsername}
+          newPassword={newPassword}
+          newNickname={newNickname}
           setNewUsername={setNewUsername}
           setNewPassword={setNewPassword}
           setNewNickname={setNewNickname}
+          setLoggedinUser={setLoggedinUser}
         />
       </>
     );
