f6500843688b2a9e2136738e3bd877b040eb2e4b
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-10 20:33:19 -0700

    Pass additional state to each Card

--- client/components/ReadingPanel.jsx
+++ client/components/ReadingPanel.jsx
@@ -2,25 +2,38 @@ import React from 'react';
 import Card from './Card.jsx';
 
 export default function ReadingPanel(props) {
-  const { showAnswer, displayAnswer } = props;
+  const { showAnswer, displayAnswer, answer, cardsDealt } = props;
   return (
     <>
       <div className='reading-panel'>
         <div className='card-container'>
-          <Card cardNumber={props.keys[0]} showAnswer={showAnswer} />
+          <Card
+            cardNumber={props.keys[0]}
+            showAnswer={showAnswer}
+            cardsDealt={cardsDealt}
+          />
           <div className='keywords'></div>
         </div>
         <div className='card-container'>
-          <Card cardNumber={props.keys[1]} showAnswer={showAnswer} />
+          <Card
+            cardNumber={props.keys[1]}
+            showAnswer={showAnswer}
+            cardsDealt={cardsDealt}
+          />
           <div className='keywords'></div>
         </div>
         <div className='card-container'>
-          <Card cardNumber={props.keys[2]} showAnswer={showAnswer} />
+          <Card
+            cardNumber={props.keys[2]}
+            showAnswer={showAnswer}
+            cardsDealt={cardsDealt}
+          />
           <div className='keywords'></div>
         </div>
       </div>
+
       <div className='divine-answer'>
-        {displayAnswer === 3 && <div className='answer'>ANSWSER!!</div>}
+        {displayAnswer === 3 && <h1 className='answer'>{answer}</h1>}
       </div>
     </>
   );
