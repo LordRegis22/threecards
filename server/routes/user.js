9d3fe12516b378d3a8d1dda4f68972d7cf94df18
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-10 20:36:24 -0700

    Add deleteReading endpoint

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.use('/login', userController.verify, (req, res) => {
-  res.status(200).send('You are logged in!');
+  res.status(200).send(res.locals.verifiedUser);
 });
 
 router.use('/create', userController.create, (req, res) => {
@@ -14,5 +14,9 @@ router.use('/create', userController.create, (req, res) => {
 
 router.use('/save', userController.saveReading, (req, res) => {
   res.status(200).send(res.locals.updatedUser);
+});
+
+router.use('/delete', userController.deleteReading, (req, res) => {
+  res.status(200).json(res.locals.updatedUser);
 });
 module.exports = router;
