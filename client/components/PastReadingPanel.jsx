8fd1e046ffa3d04fed8b9b5f103c9eac6682590d
LordRegis22 <michael.b.ohalloran@gmail.com>
2020-09-10 20:33:07 -0700

    Render array of PastReading components

--- client/components/PastReadingPanel.jsx
+++ client/components/PastReadingPanel.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import PastReading from './PastReading.jsx';
 
 function PastReadingPanel(props) {
-  const { pastReadings } = props;
-  console.log(pastReadings);
+  const { pastReadings, loggedinUser, setLoggedinUser } = props;
+
   const pastReadingsComponents = [];
   for (let el of pastReadings) {
-    pastReadingsComponents.push(<PastReading reading={el}></PastReading>);
+    pastReadingsComponents.push(
+      <PastReading
+        reading={el}
+        loggedinUser={loggedinUser}
+        setLoggedinUser={setLoggedinUser}
+      ></PastReading>
+    );
   }
-  return <div className='past-reading-panel'>{pastReadingsComponents}</div>;
+  return (
+    <>
+      <h2>Past Readings</h2>
+      <div className='past-reading-panel'>{pastReadingsComponents}</div>
+    </>
+  );
 }
 
 export default PastReadingPanel;
